36c82d16c1710a566403e4a9f00466568ed38029
habtesh-beki <habteshbaki2014@gmail.com>
2025-04-11 17:11:36 -0400

    add some socket functionality

--- server/server.ts
+++ server/server.ts
@@ -14,54 +14,71 @@ type GustType = {
   username: String;
   roomID: String;
   typing: boolean;
-  cursorPostion: number;
+  socketId: string;
+  cursorPosition: number;
 };
 
 const roomsAsAgust: GustType[] | never = [];
 
+const findRoomById = (socketId: string) => {
+  const Room = roomsAsAgust.find((room) => room.socketId == socketId)?.roomID;
+  if (!Room) {
+    throw new Error("there is no room in this id");
+  }
+  return Room;
+};
+const getUserBySocketId = (socketId: string) => {
+  const User = roomsAsAgust.find((room) => (room.socketId = socketId));
+  if (!User) {
+    throw new Error("there is not user by in this id");
+  }
+  return User;
+};
+
 io.on(SOCKET_TYPE.CONNECT, (socket) => {
   console.log("socket connected");
-  socket.on(
-    SOCKET_TYPE.JOIN,
-    (roomID: string, username: string, loginStates: string) => {
-      // if (loginStates === "gust") {
-      roomsAsAgust.map((user) => {
-        const alreadyUserExist = user.username == username;
-        if (alreadyUserExist) {
-          socket.emit(SOCKET_TYPE.USER_EXIST, "user already exist");
-        }
-        const newuser = {
-          roomID: roomID,
-          username: username,
-          typing: false,
-          cursorPostion: 0,
-        };
-        roomsAsAgust.push(newuser);
-        socket.join(roomID);
-      });
-      // }
+  socket.on(SOCKET_TYPE.JOIN, (roomID: string, username: string) => {
+    const userAlreadyExist = roomsAsAgust.filter((user) => {
+      user.username == username;
+    });
 
-      socket.on(SOCKET_TYPE.UPDATED_CODE, (newCode: string) => {
-        socket.broadcast.to(roomID).emit(SOCKET_TYPE.UPDATED_CODE, newCode);
-      });
-
-      socket.on(SOCKET_TYPE.TYPEING, (cursorPositon: number) => {
-        if (loginStates == "gust") {
-        }
-        socket.to(roomID).emit(SOCKET_TYPE.CUROSR_POSITION, cursorPositon);
-      });
-
-      socket.on(SOCKET_TYPE.PAUSE, () => {
-        const currentWriter = roomsAsAgust.filter(
-          (room) => room.roomID == roomID
-        );
-        currentWriter.map((Obj) => {});
-      });
-      socket.on(SOCKET_TYPE.LEAVE, () => {
-        socket.leave(socket.id);
-      });
+    if (userAlreadyExist) {
+      socket.emit(SOCKET_TYPE.USER_EXIST, "user already exist");
+    } else {
+      const newUser = {
+        roomID: roomID,
+        username: username,
+        typing: false,
+        socketId: socket.id,
+        cursorPosition: 0,
+      };
+      roomsAsAgust.push(newUser);
+      socket.join(roomID);
     }
-  );
+  });
+
+  socket.on(SOCKET_TYPE.UPDATED_CODE, (newCode: string) => {
+    const roomId = findRoomById(socket.id);
+    socket.broadcast.to(roomId).emit(SOCKET_TYPE.UPDATED_CODE, newCode);
+  });
+
+  socket.on(SOCKET_TYPE.TYPEING, (cursorPosition: number) => {
+    const roomId = findRoomById(socket.id);
+    socket.to(roomId).emit(SOCKET_TYPE.CUROSR_POSITION, cursorPosition);
+  });
+
+  socket.on(SOCKET_TYPE.PAUSE, () => {
+    const user = getUserBySocketId(socket.id);
+    const roomId = findRoomById(socket.id);
+    const newUser = { ...user, typing: false };
+    roomsAsAgust.push(newUser);
+    socket.to(roomId).emit(SOCKET_TYPE.PAUSE);
+  });
+
+  socket.on(SOCKET_TYPE.LEAVE, () => {
+    const roomID = findRoomById(socket.id);
+    socket.in(socket.io).socketsLeave(roomID);
+  });
 });
 
 server.listen(3000, () => {
