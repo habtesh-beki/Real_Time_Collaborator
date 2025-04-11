bb4a78767d79fb7a360680ae38ad5911595c7d6e
habtesh-beki <habteshbaki2014@gmail.com>
2025-04-11 17:12:24 -0400

    connect to backend socket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
-import "./App.css";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  useEffect(() => {
+    const socket = io("http://localhost:3000");
+    socket.on("connect", () => {
+      console.log("connect with server");
+    });
+  }, []);
+  return <div className="text-black">Hi There</div>;
 }
 
 export default App;
