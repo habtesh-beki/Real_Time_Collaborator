ca60015218c3bf7699a674da7fde72a370c439cf
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-12 13:26:08 -0400

    add better-auth functionality

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -1,13 +1,27 @@
+import { configDotenv } from "dotenv";
 import { betterAuth } from "better-auth";
+import { Pool } from "pg";
+configDotenv();
 
 export const auth = betterAuth({
+  trustedOrigins: ["http://localhost:5173"],
+  database: new Pool({
+    connectionString:
+      "postgres://postgres:habtesh01@localhost:5432/Collaborative",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      redirectURI: "http://localhost:5173/editor",
     },
-    emailAndPassword: {
-      enabled: true,
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      redirectURI: "http://localhost:5173/editor",
     },
   },
 });
