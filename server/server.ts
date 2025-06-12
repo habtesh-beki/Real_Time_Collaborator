4f9ddefb493d348cea023e712b53074fe0d82742
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-12 13:25:29 -0400

    add the cors

--- server/server.ts
+++ server/server.ts
@@ -2,10 +2,20 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { SOCKET_TYPE } from "./types";
+import { auth } from "./lib/auth";
 import LoginRoute from "./routes/login.route";
+import { toNodeHandler } from "better-auth/node";
+import cors from "cors";
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -100,6 +110,7 @@ io.on(SOCKET_TYPE.CONNECT, (socket) => {
 });
 
 app.use(LoginRoute);
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 server.listen(3000, () => {
   console.log("server listening on port 3000");
