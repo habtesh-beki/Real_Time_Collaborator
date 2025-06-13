418bd2bd6f0665b95f7dda6147175cebff76f9db
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-13 05:43:35 -0400

    change the redirect url

--- server/lib/auth.ts
+++ server/lib/auth.ts
@@ -21,7 +21,7 @@ export const auth = betterAuth({
     github: {
       clientId: process.env.GITHUB_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-      redirectURI: "http://localhost:5173/editor",
+      redirectURI: "http://localhost:5173/joinroom",
     },
   },
 });
