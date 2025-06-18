3bba9ca9d8131461b1d96706c311f1083c42c17c
habtesh-beki <habteshbaki2014@gmail.com>
2025-06-18 16:10:22 -0400

    added some futures for drawing page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,8 +4,23 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
 
+// import "@excalidraw/excalidraw/index.css";
+// import type * as TExcalidraw from "@excalidraw/excalidraw";
+
+// declare global {
+//   interface Window {
+//     ExcalidrawLib: typeof TExcalidraw;
+//   }
+// }
+
+// const { Excalidraw } = window.ExcalidrawLib;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <App
+    //  useCustom={(api: any, args?: any[]) => {}}
+    //  excalidrawLib={window.ExcalidrawLib}
+    />
+    {/* </App> */}
   </StrictMode>
 );
